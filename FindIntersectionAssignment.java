d7c733159cf64cb3105b7d548eab17a2babdfc13
sanz369 <120079851+sanz369@users.noreply.github.com>
2022-12-12 15:05:28 +0530

    Add files via upload

---
+++ FindIntersectionAssignment.java
@@ -0,0 +1,24 @@
+package week1.day2;
+
+public class FindIntersectionAssignment {
+
+	public static void main(String[] args) {
+
+			int[] Array1 = {3, 2, 11, 4, 6, 7};
+			int[] Array2 = {1, 2, 8, 4, 9, 7}; 	
+			
+
+			for (int i = 0; i < Array1.length; i++) 
+			{
+				for (int j = 0; j < Array2.length; j++) {
+
+					if (Array1[i] == Array2[j])
+					{								
+						System.out.println(Array1[i]);
+					}	
+				}			
+			}
+
+	}
+
+}
